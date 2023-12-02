fb7bd52215e2039f8cebdfb957c45bccce139480
Kenth Saya-ang <kenth.sayaang.pixel8@gmail.com>
2023-12-02 23:59:59 +0800

    moved server.py to root

--- frontend/uploadImage.js
+++ frontend/uploadImage.js
@@ -11,7 +11,7 @@ async function processImage() {
   formData.append("image", file);
 
   try {
-    const response = await fetch("http://127.0.0.1:5000/upload", {
+    const response = await fetch("http://127.0.0.1:8080/upload", {
       method: "POST",
       body: formData,
     });
