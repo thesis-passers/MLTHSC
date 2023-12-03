7d4efd7505bffdfafcfae81e65d5d9f559cd9b4b
Kenth Saya-ang <kenth.sayaang.pixel8@gmail.com>
2023-12-03 13:01:19 +0800

    fixed ocr in script

--- frontend/uploadImage.js
+++ frontend/uploadImage.js
@@ -1,64 +1,92 @@
 imageInput.addEventListener("change", () => {
-  if (imageInput.files.length === 0) {
-    Toast("Please select an image file.", "failed", false);
-    return;
-  }
+    if (imageInput.files.length === 0) {
+        Toast("Please select an image file.", "failed", false);
+        return;
+    }
 
-  const file = imageInput.files[0];
-  const fileName = file.name;
+    const file = imageInput.files[0];
+    const fileName = file.name;
 
-  fileNameElement.textContent = fileName;
-  uploadedImage.src = URL.createObjectURL(file);
-  uploadedImage.style.display = "block";
+    fileNameElement.textContent = fileName;
+    uploadedImage.src = URL.createObjectURL(file);
+    uploadedImage.style.display = "block";
 
-  const imageIcon = document.getElementById("image-icon");
-  imageIcon.style.display = "none";
+    const imageIcon = document.getElementById("image-icon");
+    imageIcon.style.display = "none";
 
-  imageBtn.disabled = false;
-  clearBtn.disabled = false;
+    imageBtn.disabled = false;
+    clearBtn.disabled = false;
 });
 
 async function processImage() {
-  const loadingToast = Toast("Extracting text please wait!", "loading", true);
+    const loadingToast = Toast("Extracting text please wait!", "loading", true);
 
-  const file = imageInput.files[0];
+    try {
+        const file = imageInput.files[0];
 
-  const formData = new FormData();
-  formData.append("image", file);
+        if (!file) {
+            handleImageProcessingError("Please select an image file.", loadingToast);
+            return;
+        }
 
-  try {
-    const response = await fetch("http://127.0.0.1:8080/upload", {
-      method: "POST",
-      body: formData,
+        const formData = createFormData(file);
+        const response = await uploadImage(formData); 
+
+
+          if (response.ok) {
+            const data = await response.json();
+            console.log(   `data: ${JSON.stringify(data, null, 2)}`)
+            handleImageExtractionSuccess(data, loadingToast);
+          } else {
+            handleImageProcessingError(`Error: ${response.statusText}`, loadingToast);
+          }
+    } catch (error) {
+        handleImageProcessingError(`Error: ${error.message}`, loadingToast);
+    }
+}
+
+function createFormData(file) {
+    const formData = new FormData();
+    formData.append("image", file);
+    console.log(file)
+    console.log(formData)
+    return formData;
+}
+
+async function uploadImage(formData) {
+    return await fetch("http://127.0.0.1:8080/upload", {
+        method: "POST",
+        body: formData,
     });
 
-    if (response.ok) {
-      const data = await response.json();
-      console.log("Hate Speech:", data.hateSpeech);
+}
 
-      const wordCount = countWords(data.postContent);
-      if (wordCount < 3 || wordCount > 280) {
+function handleImageExtractionSuccess(data, loadingToast) {
+    const wordCount = countWords(data.hateSpeech);
+
+    if (wordCount < 3 || wordCount > 280) {
         loadingToast.remove();
-        Toast(
-          "Extracted text does not satisfy the word limit.",
-          "failed",
-          false
-        );
-      } else {
+        Toast("Extracted text does not satisfy the word limit.", "failed", false);
+    } else {
         loadingToast.remove();
         Toast("Text successfully extracted!", "success", false);
+
         extractedTextbox.style.display = "block";
         extractedTextbox.value = data.hateSpeech;
 
         showAnalyzingState();
         setTimeout(() => {
-          fetchLabelsandDisplay2();
+            fetchLabelsandDisplay2();
         }, 1000);
-      }
-    } else {
-      console.error("Error:", response.statusText);
     }
-  } catch (error) {
-    console.error("Error:", error.message);
-  }
+}
+
+function handleImageProcessingError(errorMessage, loadingToast) {
+    console.error(errorMessage);
+    loadingToast.remove();
+    Toast("Error extracting text. Please try again.", "failed", false);
+}
+
+function countWords(str) {
+    return str.trim().split(/\s+/).length;
 }
